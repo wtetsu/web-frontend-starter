5095e3f5ac557660cfbdb14c81a66894bbe310d5
wtetsu <tewatana@gmail.com>
2020-06-05 23:21:57 +0900

    axios

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { Counter } from "./Counter";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+
+axios.get("data.json").then((data) => {
+  console.log(data);
+});
 
 const Main = () => {
   const [startDate, setStartDate] = useState(new Date());
